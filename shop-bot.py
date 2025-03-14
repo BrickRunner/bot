11ee01641a26e2d12ace1d640db418b15032b51b
Dmitry Mikhailiyk <dimonmix_007@mail.ru>
2025-03-14 23:09:23 +0300

    commit

--- shop-bot.py
+++ shop-bot.py
@@ -8,7 +8,17 @@ dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
+    await message.answer_sticker('CAACAgIAAxkBAANLZ9SGqQMEueUG0MW2pg1cH_KmkHkAAm8AA8GcYAzLDn2LwN1NVjYE')
     await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин спортивных товаров')
+
+@dp.message_handler(content_types=['sticker'])
+async def check_sticker(message: types.Message):
+    await message.answer(message.sticker.file_id)
+    await bot.send_message(message.from_user.id, message.chat.id)
+
+@dp.message_handler(content_types=['document', 'photo'])
+async def forward_message(message: types.Message):
+    await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
 
 @dp.message_handler()
 async def answer(message: types.Message):
