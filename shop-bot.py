6729919aa3e99d2d878ba38452b026c3ce229f8c
Dmitry Mikhailiyk <dimonmix_007@mail.ru>
2025-03-14 22:27:12 +0300

    first commit

---
+++ shop-bot.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин спортивных товаров')
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply('Нет такой команды')\
+    
+if __name__ == '__main__':
+    executor.start_polling(dp)
