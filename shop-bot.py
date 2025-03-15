dc44b81fbc70ab12ad7a42c4fd9c82fd073db76c
Dmitry Mikhailiyk <dimonmix_007@mail.ru>
2025-03-15 15:09:27 +0300

    commit

--- shop-bot.py
+++ shop-bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
 
@@ -6,23 +7,50 @@ load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
+
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Каталог').add('Специальные предложения').add('Информация о доставке').add("Контакты").add('Личный кабинет').add('Полезные советы')
+
+main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+main_admin.add('Каталог').add('Специальные предложения').add('Информация о доставке').add("Контакты").add('Личный кабинет').add('Полезные советы').add('Админ-панель')
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
+admin_panel.add('Добавить товар').add('Удалить товар')
+
+
+
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     await message.answer_sticker('CAACAgIAAxkBAANLZ9SGqQMEueUG0MW2pg1cH_KmkHkAAm8AA8GcYAzLDn2LwN1NVjYE')
-    await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин спортивных товаров')
+    await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин спортивных товаров', reply_markup=main)
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы авторизовались как администратор!', reply_markup=main_admin)
 
-@dp.message_handler(content_types=['sticker'])
-async def check_sticker(message: types.Message):
-    await message.answer(message.sticker.file_id)
-    await bot.send_message(message.from_user.id, message.chat.id)
+
+@dp.message_handler(text='Контакты')
+async def contacts(message: types.Message):
+    await message.answer(f'Для заказа пишите: @Brick_runner')
+
+
+
+@dp.message_handler(text='Админ-панель')
+async def admin(message: types.Message):
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы вошли в админ панель', reply_markup=admin_panel)
+    else:
+        await message.reply('Нет такой команды')
+
+
 
 @dp.message_handler(content_types=['document', 'photo'])
 async def forward_message(message: types.Message):
     await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
 
+    
+
 @dp.message_handler()
 async def answer(message: types.Message):
-    await message.reply('Нет такой команды')\
+    await message.reply('Нет такой команды')
     
 if __name__ == '__main__':
     executor.start_polling(dp)
